e64dfa137626402905cb4f560407f0ac9a6ace65
Andrew Dedik <swipe440@gmail.com>
2026-05-30 00:50:38 +0300

    [#7] transition from maximum to sum

--- src/player/my_player.cpp
+++ src/player/my_player.cpp
@@ -1,6 +1,7 @@
 #include "my_player.hpp"
 #include <cstdlib>
 #include <iostream>
+#include <iomanip>
 #include <vector>
 #include <algorithm>
 #include <cmath>
@@ -49,7 +50,7 @@ int  MyPlayer::check_line(const State &state, int start_x, int start_y, int dx, 
         
         Sign cell = state.get_value(curr_x, curr_y);
         if (cell == opponent_sign || cell == Sign::WALL) {
-            return -1; 
+            return 0; 
         }
         if (cell == my_sign) {
             count++;
@@ -69,10 +70,18 @@ int  MyPlayer::check_line(const State &state, int start_x, int start_y, int dx, 
     if (after_x >= 0 && after_x < cols && after_y >= 0 && after_y < rows) {
         if (state.get_value(after_x, after_y) == Sign::NONE) open_after = true;
     }
-    
-    int weight = (count > 1) ? (count - 1) : 0;
-    if (count > 1 && open_before && open_after) weight++; //+1 если открыта
-    return weight;
+    if (count > 1){
+        int weight = 1;
+        for (int i=0; i < count; i++){
+            weight*=10; 
+        }
+        if (open_before && open_after) weight *= 10; //*2 если открыта
+        return weight;
+    }
+    else{
+        return count*10;
+    }
+
 }
 
 Point MyPlayer::make_move(const State &state) {
@@ -89,7 +98,8 @@ Point MyPlayer::make_move(const State &state) {
     std::vector<RatedMovePoint> immediate_def;     
     std::vector<RatedMovePoint> immediate_open_four;           
     std::vector<RatedMovePoint> moves;     
-    
+
+    // 1) Расчет веса для пустых клеток.
     for (int y = 0; y < rows; ++y) {
         for (int x = 0; x < cols; ++x) {
             if (state.get_value(x, y) != Sign::NONE) {
@@ -98,16 +108,15 @@ Point MyPlayer::make_move(const State &state) {
             bool can_immediate_win = false;
             bool can_immediate_open_four = false;
             bool must_immediate_def = false;
-            
+
             RatedMovePoint move = { {x, y}, 0.0, 0.0, 0.0, getDistanceToCenter(x, y, rows, cols) };
-            
             for (const auto dir : directions) {            
                 int dx = dir.dx;//D_X[d];
                 int dy = dir.dy;//D_Y[d];
                 
-                int max_atk_dir_weight = 0;
-                int max_def_dir_weight = 0;
-                
+                int atk_dir_weight = 0;
+                int def_dir_weight = 0;
+
                 for (int shift = 0; shift < win_len; shift++) {
                     int start_x = x - shift * dx;
                     int start_y = y - shift * dy;
@@ -128,17 +137,12 @@ Point MyPlayer::make_move(const State &state) {
                         if (cell == m_sign) count_my++;
                     }
                     
-                    if (!blocked_my) {
+                    if (!blocked_my && count_my > 0) {
                         if (count_my == win_len - 1) { can_immediate_win = true; }
-                        if (win_len == 5 && count_my == 3) {
-                            int res = check_line(state, start_x, start_y, dx, dy, win_len, true);
+                        atk_dir_weight = check_line(state, start_x, start_y, dx, dy, win_len, true);
+                        if (count_my == win_len - 2) {
                             if (open_before && open_after) { can_immediate_open_four = true; }
-                        }
-                        
-                        int res = check_line(state, start_x, start_y, dx, dy, win_len, true);
-                        if (res > max_atk_dir_weight) {
-                            max_atk_dir_weight = res;
-                        }
+                        }                        
                     }
                     
                     // Анализ Защиты
@@ -150,22 +154,17 @@ Point MyPlayer::make_move(const State &state) {
                         if (cell == opponent_sign) count_op++;
                     }
                     
-                    if (!blocked_op) {
-                        if (count_op == win_len - 1) { must_immediate_def = true; }
-                        
-                        int res = check_line(state, start_x, start_y, dx, dy, win_len, false);
-                        if (res > max_def_dir_weight) {
-                            max_def_dir_weight = res;
-                        }
+                    if (!blocked_op && count_op > 0) {
+                        if (count_op == win_len - 1) { must_immediate_def = true; }                        
+                        def_dir_weight = check_line(state, start_x, start_y, dx, dy, win_len, false);
                     }
+
+                    move.atk_weight += atk_dir_weight;
+                    move.def_weight += def_dir_weight;
                 }
                 
-                move.atk_weight += max_atk_dir_weight;
-                move.def_weight += max_def_dir_weight;
             }
-            
             move.weight = (move.atk_weight * ATK_COEFF) + (move.def_weight * DEF_COEFF);
-            
             if (can_immediate_win)       immediate_win.push_back(move);
             if (can_immediate_open_four) immediate_open_four.push_back(move);
             if (must_immediate_def)      immediate_def.push_back(move);
@@ -175,42 +174,115 @@ Point MyPlayer::make_move(const State &state) {
     }
     
     // для сортировки векторов
-    auto move_comparator = [](const RatedMovePoint& a, const RatedMovePoint& b) {
+    auto weight_comparator = [](const RatedMovePoint& a, const RatedMovePoint& b) {
         if (std::abs(a.weight - b.weight) > 0.0001) {
             return a.weight > b.weight;
         }
         return a.distance < b.distance;
     };
-    
-    Point result {0, 0};  
+    auto atk_weight_comparator = [](const RatedMovePoint& a, const RatedMovePoint& b) {
+        if (std::abs(a.atk_weight - b.atk_weight) > 0.0001) {
+            return a.atk_weight > b.atk_weight;
+        }
+        return a.distance < b.distance;
+    };
+    auto def_weight_comparator = [](const RatedMovePoint& a, const RatedMovePoint& b) {
+        if (std::abs(a.def_weight - b.def_weight) > 0.0001) {
+            return a.def_weight > b.def_weight;
+        }
+        return a.distance < b.distance;
+    };
+         
     // ВЫБОР ХОДА ПО ПРАВИЛАМ
-    // 1) Проверка на победу. Ищется клетка, которая завершит собственную линию до длины L. То есть нужно найти линию L-1, которую можно завершить. Если такая клетка есть, ход делается туда.
+    // 2) Проверка на победу. Ищется клетка, которая завершит собственную линию до длины L. 
+    //    То есть нужно найти линию L-1, которую можно завершить. Если такая клетка есть, ход делается туда. 
+    //    Если таких клеток несколько выбираем с большим весом защиты.
     if (!immediate_win.empty()) {
-        std::sort(immediate_win.begin(), immediate_win.end(), move_comparator);
-        result=immediate_win[0].point;
+        std::sort(immediate_win.begin(), immediate_win.end(), atk_weight_comparator);
+        return immediate_win[0].point;
     }
-    // 2) Предотвращение проигрыша. Ищется клетка, которая позволит сопернику следующим ходом завершить линию до длины L. То есть нужно найти линию L-1, которую можно завершить. Если такая клетка есть, ход делается туда, чтобы её занять.
+    // 3) Предотвращение проигрыша. Ищется клетка, которая позволит сопернику следующим ходом завершить линию до длины L. 
+    //    То есть нужно найти линию L-1, которую можно завершить. Если такая клетка есть, ход делается туда, чтобы её занять. 
+    //    Если таких клеток несколько выбираем с большим весом атаки.
     if (!immediate_def.empty()) {
-        std::sort(immediate_def.begin(), immediate_def.end(), move_comparator);
-        result=immediate_def[0].point;
+        std::sort(immediate_def.begin(), immediate_def.end(), def_weight_comparator);
+        return immediate_def[0].point;
     }
-    // 3) Проверка на победную четверку. Проверить можно ли сделать 4 в ряд с пустыми на концах. Комбинация ведёт к выигрышу на следующем ходу. Если есть, то делаем.
+    // 4) Проверка на победную четверку. Проверить можно ли сделать 4 в ряд с пустыми на концах. 
+    //    Комбинация ведёт к выигрышу на следующем ходу. Если есть, то делаем. 
+    //    Если таких клеток несколько выбираем с большим весом защиты.
     if (!immediate_open_four.empty()) {
-        std::sort(immediate_open_four.begin(), immediate_open_four.end(), move_comparator);
-        result=immediate_open_four[0].point;
+        std::sort(immediate_open_four.begin(), immediate_open_four.end(), atk_weight_comparator);
+        return immediate_open_four[0].point;
     }
-    // 8) Из всех свободных клеток выбирается та, у которой итоговый вес оказался максимальным. Если таких несколько, то выбираем клетку ближе к центру поля.
+    // 5) Из всех свободных клеток выбирается та, у которой итоговый вес оказался максимальным. 
+    //    Если таких несколько, то выбираем клетку ближе к центру поля. Но если есть несколько клеток 
+    //    с одинаковым весом в одной линии и их атакующий вес больше защитного, то ставим X не подряд, а с другого конца линии.
     if (!moves.empty()) {
-        std::sort(moves.begin(), moves.end(), move_comparator);
-        result=moves[0].point;
-        //if (m_sign==Sign::X){
-        //    std::cout<<"XXX x="<<result.x<<" y="<<result.y<<" a="<<moves[0].atk_weight<<" d="<<moves[0].def_weight<<std::endl;
-        //}
-        //else{
-        //    std::cout<<"OOO x="<<result.x<<" y="<<result.y<<" a="<<moves[0].atk_weight<<" d="<<moves[0].def_weight<<std::endl;
-        //}
+        std::sort(moves.begin(), moves.end(), weight_comparator);
+        //return moves[0].point;
+        double first_weight = moves[0].weight;
+        std::vector<RatedMovePoint> atk_moves; // Все атакующие ходы, делящие максимальный балл
+        for (const auto& m : moves) {            
+            if (std::abs(m.weight - first_weight) < 0.0001) {
+                if (m.atk_weight > m.def_weight) atk_moves.push_back(m);
+            } else {
+                break; // Массив отсортирован по убыванию, дальше веса меньше
+            }
+        }
+        if (atk_moves.size() > 1) {
+           for (int dis = win_len-1; dis >= 0; dis--) { //цикл по дистанции между клетками от макс (win_len-1) до мин (0)
+             for (size_t i = 0; i < atk_moves.size(); i++) {
+                for (size_t j = i + 1; j < atk_moves.size(); j++) {
+                    const auto& m1 = atk_moves[i];
+                    const auto& m2 = atk_moves[j];
+                    int dx = m2.point.x - m1.point.x;
+                    int dy = m2.point.y - m1.point.y;
+                    bool same_line = (dx == 0 || dy == 0 || std::abs(dx) == std::abs(dy));// m1 и m2 на одной линии
+                    int distance = std::max(std::abs(dx), std::abs(dy));// расстояние между m1 и m2
+                    if (same_line && distance == dis) {
+                        int step_x = (dx > 0) ? 1 : ((dx < 0) ? -1 : 0);
+                        int step_y = (dy > 0) ? 1 : ((dy < 0) ? -1 : 0);
+                        int near_m1 = 0; // количество наших крестиков рядом с m1
+                        int near_m2 = 0; // количество наших крестиков рядом с m2
+                        // Проверяем клетки, которая находится рядом с точкой m1
+                        int check_x1 = m1.point.x + step_x;
+                        int check_y1 = m1.point.y + step_y;
+                        int check_x2 = m1.point.x - step_x;
+                        int check_y2 = m1.point.y - step_y;
+                        if (check_x1 >= 0 && check_x1 < cols && check_y1 >= 0 && check_y1 < rows) {
+                            if (state.get_value(check_x1, check_y1)== m_sign) near_m1++;                            
+                        }
+                        if (check_x2 >= 0 && check_x2 < cols && check_y2 >= 0 && check_y2 < rows) {
+                            if (state.get_value(check_x2, check_y2)== m_sign) near_m1++;
+                        }
+                        // Проверяем клетки, которая находится рядом с точкой m2
+                        check_x1 = m2.point.x + step_x;
+                        check_y1 = m2.point.y + step_y;
+                        check_x2 = m2.point.x - step_x;
+                        check_y2 = m2.point.y - step_y;
+                        if (check_x1 >= 0 && check_x1 < cols && check_y1 >= 0 && check_y1 < rows) {
+                            if (state.get_value(check_x1, check_y1)== m_sign) near_m2++;                     
+                        }
+                        if (check_x2 >= 0 && check_x2 < cols && check_y2 >= 0 && check_y2 < rows) {
+                            if (state.get_value(check_x2, check_y2)== m_sign) near_m2++;
+                        }
+                        if (near_m1 > near_m2) {
+                            // Если рядом с m1 наших крестиков больше чем у m2, значит m2 — это другой свободный конец линии!
+                            return m2.point;
+                        }
+                        else{
+                            // Если рядом с m2 наших крестиков больше чем у m1, значит m1 — это другой свободный конец линии!
+                            return m1.point;
+                        }
+                    }
+                }
+             }
+           }
+        }
+        return moves[0].point;
     }    
-    return result;   
+    return Point{0, 0}; // нет свободных клеток, возвращаем 0,0 
 }
 
 void MyPlayer2::set_sign(Sign sign) { m_sign = sign; }
@@ -242,9 +314,6 @@ Point MyPlayer2::make_move(const State &state) {
     if (has_neighbors)
       break;
   }
-  //result.x=2;
-  //result.y=temp++;
-  //if (temp==4) temp++;
   return result;
 }
 
